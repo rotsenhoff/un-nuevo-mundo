c6b9a4386b74c4b6ea02581d1adf68dbf4ba4e40
rotsenhoff <nest87granmar@gmail.com>
2025-03-05 18:16:51 +0100

    version8.5

---
+++ PYTHON/Test prácico.py
@@ -0,0 +1,124 @@
+
+# Dados dos números, escriba un código Python para encontrar el mínimo de estos dos números
+
+def minimum(a,b):
+
+    if a <= b:
+        return a
+    else:
+        return b
+    
+a = 2
+b = 4
+print (minimum(a,b))
+
+# Invertir palabras de una cadena dada
+
+def rev_sentence(sentence):
+
+    words = sentence.split(' ')
+
+    reverse_sentence = ' '.join(reversed(words))
+
+    return reverse_sentence
+
+if __name__ == "__main__":
+    input = 'geeks quiz practice code'
+    print (rev_sentence(input))
+
+
+# Realiza una suma de los elementos de una tupla
+
+test_tup = (7, 8, 9, 1, 10, 7)
+
+print("The original tuple is: " + str(test_tup))
+
+res = sum(list(test_tup))
+
+print("The summation of tuple elements are : " + str(res))
+
+# Escribe un código que calcule una lista de números proporcionados
+
+def list_sum(num_List):
+    
+    if len(num_List) == 1:
+        return num_List[0]
+    else:
+        return num_List[0] + list_sum(num_List[1:])
+print(list_sum([3,5, 8, 9, 9]))
+
+# Escriba un código que desordene al azar una lista
+
+from random import shuffle
+x = ['Skyrim', 'Pertenece', 'A', 'Los', 'Nórdicos']
+shuffle (x)
+print(x)
+
+# Escribaun código que pueda contar todas las palabras mayúsculas de un archivo
+
+with open(archivo) as fh:
+    count = 0
+text = fh.read()
+for character in text:
+    if character.isupper():
+        count += 1
+
+# Escriba un programa para producir la serie Fibonacci en Python.
+
+n=int(input("Intriduzca el número de valores de 'n': "))
+first=0
+second=1
+sum=0
+count=1
+print("Secuencia de Fibonacci: ")
+while(count<=n):
+    print(sum)
+    count+=1
+    first=second
+    second=sum
+    sum=first+second
+
+# Escriba un programa en Python para comprobar si un número es primo.
+
+def isPrimo(num):
+    if num < 1:
+        return False
+    elif num == 2:
+        return True
+    else:
+        for i in range(2, num):
+            if num % i == 0:
+                return False
+        return True
+    
+def app():
+    num = int(input('Escriba un número: '))
+    result = isPrimo(num)
+
+    if result is True:
+        print('El número es primo!!')
+    else:
+        print('El número no es primo!!')
+
+if __name__ == '__main__':
+    app()
+
+# Escribir un programa en Python para comprobar si un número es capicúa. 
+# Es decir, si se lee igual de derecha a izquierda que de izquierda a derecha.
+
+a=input("introduzca un número: ")
+b=a[::-1]
+if a==b:
+    print("Es capicúa")
+else:
+    print("No es capicúa")
+
+# Escribir un algoritmo de ordenación para un conjunto de datos numéricos en Python.
+
+list = ["1", "4", "0", "6", "9"]
+list = [int(i) for i in list]
+list.sort()
+print (list)
+
+
+
--- PYTHON/tonto.py
+++
@@ -1,9 +0,0 @@
-
-name = input ('Pon tu nombre') 
-
-print (name)
-
-if [name] == 'Ela'
-
-    
-else ('has acewrtado!')
